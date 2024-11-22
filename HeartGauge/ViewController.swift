6ac780ec82529f3dbae7c081d44096eb773babca
Kenny <kennymaylla@gmail.com>
2024-11-22 13:07:21 -0500

    Initial Commit

---
+++ HeartGauge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeartGauge
+//
+//  Created by Kenny Mayancela Aylla on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
