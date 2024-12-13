2432e48815b804aab40ffbae9cd9b928213d9c01
Kenny <kennymaylla@gmail.com>
2024-12-13 14:18:38 -0500

    firebase cont.

--- HeartGauge/DashboardViewController.swift
+++ HeartGauge/DashboardViewController.swift
@@ -11,7 +11,7 @@ class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.backBarButtonItem?.isHidden = true
         // Do any additional setup after loading the view.
     }
     
