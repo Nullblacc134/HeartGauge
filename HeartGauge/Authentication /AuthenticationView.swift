a0183d2d0223562cff90fe88e8db37d608c8e683
Kenny <kennymaylla@gmail.com>
2024-12-17 14:27:23 -0500

    added a AuthentaicView and SignInWithEmailView

---
+++ HeartGauge/Authentication /AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  HeartGauge
+//
+//  Created by Kenny Mayancela Aylla on 12/17/24.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
